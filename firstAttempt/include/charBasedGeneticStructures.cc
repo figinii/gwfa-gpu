28d7ec2be2263fff1507d55bc140fcd7f63a3771
figinii <matteo5.figini@mail.polimi.it>
2025-03-17 22:50:07 +0100

    feat: LCP implementation over GeneticStrChar

--- firstAttempt/include/charBasedGeneticStructures.cc
+++ firstAttempt/include/charBasedGeneticStructures.cc
@@ -2,25 +2,42 @@
 #define CHAR_GENETIC_STRUCTURE_H
 
 #include "geneticStructures.h"
+#include "lcp.h"
 
-class GeneticStrChar : public GeneticStr {
+class GeneticStrChar : public GeneticStr<GeneticStrChar> {
 private:
-  std::string content;
-
 public:
+  char* content;
   GeneticStrChar(){
-    content = "";
+    content = (char*) malloc(sizeof(char));
   }
 
   GeneticStrChar(seqan3::dna5_vector sequence){
-    content = "";
+    content = (char*) malloc(sizeof(char) * (sequence.size()+1));
+    int i = 0;
     for(auto nucleotide : sequence){
-      content += nucleotide.to_char();
+      char tmp = nucleotide.to_char();
+      content[i] = tmp;
+      i++;
     }
   }
 
   GeneticStrChar(std::string sequence){
-    content = sequence;
+    content = (char*) malloc(sizeof(char) * (sequence.size()+1));
+    int i = 0;
+    for(auto nucleotide : sequence){
+      content[i] = nucleotide;
+      i++;
+    }
+  }
+
+  long lcp(GeneticStrChar* query) const override {
+    long len = 0;    
+    while(content[len] != '\0' && 
+          query->content[len] != '\0' &&
+          content[len] == query->content[len])
+      len++;
+    return len;
   }
 
   std::string toString() const override{
@@ -28,7 +45,7 @@ public:
   }
 
   ~GeneticStrChar() override{
-    content.clear();
+    free(content);
   }
 
 };
